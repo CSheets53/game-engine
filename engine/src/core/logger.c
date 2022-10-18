0f511351b148b62f34e1aa1c03f817e1beb696b1
Cooper Sheets <coopersheets@gmail.com>
2022-10-18 18:30:31 -0500

    Configured logging + asserts

---
+++ engine/src/core/logger.c
@@ -0,0 +1,49 @@
+#include "asserts.h"
+#include "logger.h"
+
+// TODO: temp
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+void report_assertion_failure(const char* expression, const char* message, const char* file, i32 line) {
+    log_output(LOG_LEVEL_FATAL, "Assertion Failure: %s, message: '%s', in file: %s, line: %d\n", expression, message, file, line);
+}
+
+b8 initialize_logging() {
+    // TODO: create log file
+    return TRUE;
+}
+
+void shutdown_logging() {
+    // TODO: cleanup logging/write queued entries
+}
+
+void log_output(log_level level, const char* message, ...) {
+    const char* level_strings[6] = {
+        "[FATAL]: ",
+        "[ERROR]: ",
+        "[WARN]:  ",
+        "[INFO]:  ",
+        "[DEBUG]: ",
+        "[TRACE]: "
+    };
+    // b8 is_error = level < 2;
+
+    // NOTE: imposes a 32k char limit on a single log entry to avoid dynamic allocation
+    char out_message[32000];
+    memset(out_message, 0, sizeof(out_message));
+
+    // Format original message
+    __builtin_va_list arg_ptr;
+    va_start(arg_ptr, message);
+    vsnprintf(out_message, 32000, message, arg_ptr);
+    va_end(arg_ptr);
+
+    // Append log level to the front of our message
+    char out_message2[32000];
+    sprintf(out_message2, "%s%s\n", level_strings[level], out_message);
+
+    // TODO: platform-specific output
+    printf("%s", out_message2);
+}
