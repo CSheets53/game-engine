ebd7f4790454c9e3d3c9b6999151d9484ba779a8
Cooper Sheets <coopersheets@gmail.com>
2022-10-19 08:34:59 -0500

    Built out windows platform layer

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,5 +1,6 @@
 #include "asserts.h"
 #include "logger.h"
+#include "platform/platform.h"
 
 // TODO: temp
 #include <stdarg.h>
@@ -28,22 +29,27 @@ void log_output(log_level level, const char* message, ...) {
         "[DEBUG]: ",
         "[TRACE]: "
     };
-    // b8 is_error = level < 2;
+    b8 is_error = level < LOG_LEVEL_WARN;
 
     // NOTE: imposes a 32k char limit on a single log entry to avoid dynamic allocation
-    char out_message[32000];
+    const i32 msg_length = 32000;
+    char out_message[msg_length];
     memset(out_message, 0, sizeof(out_message));
 
     // Format original message
     __builtin_va_list arg_ptr;
     va_start(arg_ptr, message);
-    vsnprintf(out_message, 32000, message, arg_ptr);
+    vsnprintf(out_message, msg_length, message, arg_ptr);
     va_end(arg_ptr);
 
     // Append log level to the front of our message
-    char out_message2[32000];
+    char out_message2[msg_length];
     sprintf(out_message2, "%s%s\n", level_strings[level], out_message);
 
-    // TODO: platform-specific output
-    printf("%s", out_message2);
+    // Platform-specific output
+    if (is_error) {
+        platform_console_write_error(out_message2, level);
+    } else {
+        platform_console_write(out_message2, level);
+    }
 }
